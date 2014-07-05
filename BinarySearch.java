980614bec1d96f8a9b4d017b82f8b0d25a9de83c
Asif Khan <23imak@gmail.com>
2014-07-05 14:44:27 +0530

    New Commit

---
+++ BinarySearch.java
@@ -0,0 +1,46 @@
+public class BinarySearch {
+	private int[] array;
+	
+
+	public BinarySearch(int[] values) {
+		this.array = values;
+	}
+/**
+ * 
+ * @param search
+ * @param option
+ * @return
+ */
+	// 
+	public int searchNumber(int search,String option) {
+		int start = 0;
+		int position = -1;
+		int end = array.length - 1;
+		while (start <= end) {
+			
+			int mid = (start + end) / 2;
+			if (search == array[mid]) {
+				position = mid;
+				if(option.equalsIgnoreCase("first")){
+					
+						end = mid-1;
+					
+					}
+				else if(option.equalsIgnoreCase("last")){
+				
+						start = mid+1;
+				}else
+					return position;
+				
+			}
+			if (search < array[mid]) {
+				end = mid - 1;
+			} else {
+				start = mid + 1;
+			}
+		}
+		return position;
+
+	}
+	
+}
