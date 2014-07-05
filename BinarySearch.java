7680af623de2daeb3c1088b9a3415fcfa37198de
Asif Khan <23imak@gmail.com>
2014-07-05 12:14:56 +0530

    New Files

---
+++ BinarySearch.java
@@ -0,0 +1,26 @@
+public class BinarySearch {
+	private int[] array;
+
+	public BinarySearch(int[] values) {
+		this.array = values;
+	}
+
+	public int searchNumber(int search) {
+		int start = 0;
+		int end = array.length - 1;
+		while (start <= end) {
+			int mid = (start + end) / 2;
+			if (search == array[mid]) {
+				return mid;
+			}
+			if (search < array[mid]) {
+				end = mid - 1;
+			} else {
+				start = mid + 1;
+			}
+		}
+		return -1;
+
+	}
+
+}
